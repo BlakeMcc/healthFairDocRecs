c1f465628f2df29906c3f0511f78876b96969fff
pjsier <pjsier@gmail.com>
2017-05-13 18:01:47 -0500

    add pagination

--- health_fair/urls.py
+++ health_fair/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^dj-admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^search/(?P<slug>[a-zA-Z0-9-]+)$', ScreenView.as_view(), name='screen'),
+    url(r'^detail/(?P<npi>[a-zA-Z0-9-]+)$', ProviderDetailView.as_view(), name='detail'),
     url(r'^send-text/(?P<slug>[a-zA-Z0-9-]+)$', SendTextView.as_view(), name='send_text')
 ]
