e6e2ed13006d8436bd3d20398f0a6b9ce64e10eb
pjsier <pjsier@gmail.com>
2017-05-14 11:36:20 -0500

    Basically working

--- health_fair/settings/prod.py
+++ health_fair/settings/prod.py
@@ -8,57 +8,57 @@ else:
     ENVIRONMENT = 'production'
     DEBUG = False
 
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-            }
-        },
-        'formatters': {
-            'verbose': {
-                'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-                'datefmt': "%d/%b/%Y %H:%M:%S"
-            },
-            'simple': {
-                'format': '%(levelname)s %(message)s'
-            },
-        },
-        'handlers': {
-            'mail_admins': {
-                'level': 'ERROR',
-                'filters': ['require_debug_false'],
-                'class': 'django.utils.log.AdminEmailHandler'
-            },
-            'error': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': os.path.join(BASE_DIR, 'health_fair', 'log', 'error.log'),
-                'formatter': 'verbose',
-            },
-            'screener': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': os.path.join(BASE_DIR, 'health_fair', 'log', 'screener.log'),
-                'formatter': 'verbose',
-            }
-        },
-        'loggers': {
-            'django': {
-                'handlers': ['error', 'mail_admins'],
-                'level': 'ERROR',
-                'propagate': True,
-            },
-            'django.request': {
-                'handlers': ['error'],
-                'level': 'ERROR',
-                'propagate': True,
-            },
-            'screener': {
-                'handlers': ['screener'],
-                'level': 'DEBUG',
-                'propagate': True,
-            }
-        }
-    }
+    # LOGGING = {
+    #     'version': 1,
+    #     'disable_existing_loggers': False,
+    #     'filters': {
+    #         'require_debug_false': {
+    #             '()': 'django.utils.log.RequireDebugFalse'
+    #         }
+    #     },
+    #     'formatters': {
+    #         'verbose': {
+    #             'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+    #             'datefmt': "%d/%b/%Y %H:%M:%S"
+    #         },
+    #         'simple': {
+    #             'format': '%(levelname)s %(message)s'
+    #         },
+    #     },
+    #     'handlers': {
+    #         'mail_admins': {
+    #             'level': 'ERROR',
+    #             'filters': ['require_debug_false'],
+    #             'class': 'django.utils.log.AdminEmailHandler'
+    #         },
+    #         'error': {
+    #             'level': 'DEBUG',
+    #             'class': 'logging.FileHandler',
+    #             'filename': os.path.join(BASE_DIR, 'health_fair', 'log', 'error.log'),
+    #             'formatter': 'verbose',
+    #         },
+    #         'screener': {
+    #             'level': 'DEBUG',
+    #             'class': 'logging.FileHandler',
+    #             'filename': os.path.join(BASE_DIR, 'health_fair', 'log', 'screener.log'),
+    #             'formatter': 'verbose',
+    #         }
+    #     },
+    #     'loggers': {
+    #         'django': {
+    #             'handlers': ['error', 'mail_admins'],
+    #             'level': 'ERROR',
+    #             'propagate': True,
+    #         },
+    #         'django.request': {
+    #             'handlers': ['error'],
+    #             'level': 'ERROR',
+    #             'propagate': True,
+    #         },
+    #         'screener': {
+    #             'handlers': ['screener'],
+    #             'level': 'DEBUG',
+    #             'propagate': True,
+    #         }
+    #     }
+    # }
